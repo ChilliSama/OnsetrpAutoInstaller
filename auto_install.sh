42c22aea6c17c0f2fe663233ba9bb5dd18106771
ChilliSama <chilli380killer@gmail.com>
2019-12-15 02:24:07 +0100

    Add basic root verification to start the script

--- auto_install.sh
+++ auto_install.sh
@@ -1,2 +1,24 @@
-#!/bin/bash
-#Awesome !
+#!/bin/bash     
+#title           :auto_install.sh
+#description     :This script will make a Onset server automaticaly on linux server.
+#author		     :Chilli
+#date            :12/15/2019
+#version         :1.0
+#usage		     :./auto_install.sh
+#notes           :Make sure you are on Ubuntu 18.04, Debian 9 or CentOS 7.
+#===================================================================================#
+
+#Global variable
+ROOT_UID=0     # Only users with $UID 0 have root privileges.
+E_NOTROOT=87   # Non-root exit error.
+LANGAGE=0      # [FR]LANGAGE=0 , [EN]LANGAGE=1
+
+#Translation - ${exemple[0]} to french
+ROOT_REQUIRED={"\e[7Vous devez être en root pour lancer le scrip." "\e[7Must be root to run this script."}
+
+# Run as root, of course.
+if [ "$UID" -ne "$ROOT_UID" ]
+then
+  echo ${ROOT_REQUIRED["$LANGAGE"]}
+  exit $E_NOTROOT
+fi  
