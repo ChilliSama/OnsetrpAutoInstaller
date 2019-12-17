6fc8fdcdfcc73248e9def624677781b9ca9ac7ef
ChilliSama <chilli380killer@gmail.com>
2019-12-17 02:44:08 +0100

    [Test Update] creating temp user to processe the installation corectly

--- auto_install.sh
+++ auto_install.sh
@@ -15,7 +15,9 @@ LANGUAGE=0     # [FR]LANGUAGE=0 , [EN]LANGUAGE=1
 ERROR=1        # Exit with error status
 SUCCESS=0      # Exit with success status
 CH='[a-zA-Z]'  # Character range in ascii to compare it 
-INT='^[0-9]+$' # Integer range in ascii to compare it 
+INT='^[0-9]+$' # Integer range in ascii to compare it
+USERTMP="tmp_user"
+PSWD="tmp_password"
 
 # Translation - ${exemple[0]} to french #
 declare -a root_required=("\e[7mVous devez être en root pour lancer le scrip.\e[0m" "\e[7Must be root to run this script.\e[0m")
@@ -32,13 +34,19 @@ declare -a install_nano=("\e[7mInstallation de nano [2/3]...\e[0m" "\e[7mNano in
 declare -a done_nano=("\e[7mStatut de nano : \e[32mInstallé\e[0m" "\e[7mNano status : \e[32mInstalled\e[0m")
 declare -a install_openssl=("\e[7mInstallation de openssl [3/3]...\e[0m" "\e[7mOpenssl instalation [3/3]...\e[0m")
 declare -a done_openssl=("\e[7mStatut de openssl : \e[32mInstallé\e[0m" "\e[7mOpenssl status : \e[32mInstalled\e[0m")
-declare -a request_curl=("Voulez vous installer Curl ? [o/n] :  " "Do you want to install Curl ? [y/n] : ")
+declare -a request_curl=("Voulez vous installer Curl ? [o/n] : " "Do you want to install Curl ? [y/n] : ")
 declare -a request_nano=("Voulez vous installer Nano ? [o/n] : " "Do you want to install Nano ? [y/n] : ")
 declare -a request_openssl=("Voulez vous installer Openssl ? [o/n] : " "Do you want to install Openssl ? [y/n] : ")
 declare -a awnser_trad=('[o-on-n]' '[y-yn-n]')
 declare -a remake_read=("Veuillez écrire [o]ui ou [n]on -> [o/n] : " "Please write [y]es ou [n]ot -> [y/n] : ")
 declare -a error_read=("Veuillez relancer le scrip en étant sur de bien répondre aux questions." "Please run the script again and make sure to correctly awnser to the question.")
-declare -a request_openssl=("Voulez vous installer Steam et Onset ? [o/n] : " "Do you want to install Steam and Onset ? [y/n] : ")
+declare -a request_steamcmd=("Voulez vous installer Steamcmd et Onset ? [o/n] : " "Do you want to install Steamcmd and Onset ? [y/n] : ")
+declare -a install_gcc=("\e[7mInstallation de GCC [3/3]...\e[0m" "\e[7mGCC instalation [3/3]...\e[0m")
+declare -a done_gcc=("\e[7mStatut de GCC : \e[32mInstallé\e[0m" "\e[7mGCC status : \e[32mInstalled\e[0m")
+declare -a install_sudo=("\e[7mInstallation de Sudo [3/3]...\e[0m" "\e[7mSudo instalation [3/3]...\e[0m")
+declare -a done_sudo=("\e[7mStatut de Sudo : \e[32mInstallé\e[0m" "\e[7mSudo status : \e[32mInstalled\e[0m")
+declare -a user_create=("\e[92m    C\e[0reation d'un nouvelle utilisateur temporaire.\e[0m" "\e[92m    C\e[0reation of temporary user.\e[0m")
+declare -a user_delete=("\e[92m    S\e[0upression de l'utilisateur temporaire.\e[0m" "\e[92m    D\e[0eleting temporary user.\e[0m")
 
 clear
 #===================================================================================#
@@ -191,12 +199,28 @@ then
         fi
     fi
 fi
-if ["$check_r" = "o"]
+if [ "$check_r" == "o" ]
 then
     echo -e ${install_curl[$LANGUAGE]}
     apt -y install curl
     echo -e ${done_curl[$LANGUAGE]}
     echo
+else
+then
+    echo -n ${remake_read[$LANGUAGE]}
+    read check_r
+    echo
+    if [ "$check_r" == "o" ]
+    then
+        echo -e ${install_curl[$LANGUAGE]}
+        apt -y install curl
+        echo -e ${done_curl[$LANGUAGE]}
+        echo
+    else
+    then
+        echo ${error_read[$LANGUAGE]}
+        exit $ERROR
+    fi
 fi
 # Nano #
 echo
@@ -223,12 +247,28 @@ then
         fi
     fi
 fi
-if ["$check_r" = "o"]
+if [ "$check_r" == "o" ]
 then
     echo -e ${install_nano[$LANGUAGE]}
     apt -y install nano
     echo -e ${done_nano[$LANGUAGE]}
     echo
+else
+then
+    echo -n ${remake_read[$LANGUAGE]}
+    read check_r
+    echo
+    if [ "$check_r" == "o" ]
+    then
+        echo -e ${install_nano[$LANGUAGE]}
+        apt -y install nano
+        echo -e ${done_nano[$LANGUAGE]}
+        echo
+    else
+    then
+        echo ${error_read[$LANGUAGE]}
+        exit $ERROR
+    fi
 fi
 # Openssl #
 echo
@@ -255,13 +295,110 @@ then
         fi
     fi
 fi
-if ["$check_r" = "o"]
+if [ "$check_r" == "o" ]
 then
     echo -e ${install_openssl[$LANGUAGE]}
     apt -y install openssl
     echo -e ${done_openssl[$LANGUAGE]}
     echo
+else
+then
+    echo -n ${remake_read[$LANGUAGE]}
+    read check_r
+    echo
+    if [ "$check_r" == "o" ]
+    then
+        echo -e ${install_openssl[$LANGUAGE]}
+        apt -y install openssl
+        echo -e ${done_openssl[$LANGUAGE]}
+        echo
+    else
+    then
+        echo ${error_read[$LANGUAGE]}
+        exit $ERROR
+    fi
 fi
 
 #===================================================================================#
 # Steamcmd #
+echo
+echo -n ${request_steamcmd[$LANGUAGE]}
+read check_r
+if ! [[ "$check_r" =~ $CH ]]
+then
+    echo -n ${remake_read[$LANGUAGE]}
+    read check_r
+    echo
+    if ! [[ "$check_r" =~ $CH ]]
+    then
+        echo ${error_read[$LANGUAGE]}
+        exit $ERROR
+    elif [["$check_r" =~ ${awnser_trad[$LANGUAGE]}]]
+    then
+        echo -n ${remake_read[$LANGUAGE]}
+        read check_r
+        echo
+        if [["$check_r" =~ ${awnser_trad[$LANGUAGE]}]]
+        then
+            echo ${error_read[$LANGUAGE]}
+            exit $ERROR
+        fi
+    fi
+fi
+if [ "$check_r" == "o" ]
+then
+    echo -e ${install_gcc[$LANGUAGE]}
+    apt-get -y install lib32gcc1
+    echo -e ${done_gcc[$LANGUAGE]}
+    echo
+    echo -e ${install_sudo[$LANGUAGE]}
+    apt-get -y install sudo
+    echo -e ${done_sudo[$LANGUAGE]}
+    echo
+    #===================================================================================#
+    # Create sudo user #
+    # just for output style #
+    for i in {232..255}
+    do
+        echo -en "\e[38;5;${i}m-\e[0m"
+    done
+    echo -en "\e[38;5;255m----------------\e[0m"
+    for i in {255..232}
+    do
+        echo -en "\e[38;5;${i}m-\e[0m"
+    done
+    echo
+
+    #===================================================================================#
+    
+    echo -e ${user_create[$LANGUAGE]}
+    echo
+
+    #===================================================================================#
+    # just for output style #
+    for i in {232..255}
+    do
+        echo -en "\e[38;5;${i}m-\e[0m"
+    done
+    echo -en "\e[38;5;255m----------------\e[0m"
+    for i in {255..232}
+    do
+        echo -en "\e[38;5;${i}m-\e[0m"
+    done
+    echo
+
+    #===================================================================================#
+
+    adduser -p $PSWD $USERTMP
+    usermod -aG sudo $USERTMP
+    su $USERTMP -p $PSWD
+    echo "test"
+    echo "$PSWD" | sudo -S deluser $USERTMP
+
+    #===================================================================================#
+
+else
+then
+    echo ${error_read[$LANGUAGE]}
+    exit $ERROR
+fi
