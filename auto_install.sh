8dcb27b182f613f3df9bc9cf61c464f52fbb2e48
ChilliSama <chilli380killer@gmail.com>
2019-12-17 15:21:12 +0100

    update language

--- auto_install.sh
+++ auto_install.sh
@@ -11,7 +11,7 @@
 # Global variable #
 ROOT_UID=0     # Only users with $UID 0 have root privileges.
 E_NOTROOT=87   # Non-root exit error.
-LANGUAGE=0     # [FR]LANGUAGE=0 , [EN]LANGUAGE=1
+LANGUAGE=1     # [FR]LANGUAGE=0 , [EN]LANGUAGE=1
 ERROR=1        # Exit with error status
 SUCCESS=0      # Exit with success status
 CH='[a-zA-Z]'  # Character range in ascii to compare it 
@@ -20,7 +20,7 @@ USERTMP="tmp_user"
 PSWD="tmp_password"
 
 # Translation - ${exemple[0]} to french #
-declare -a root_required=("\e[7mVous devez être en root pour lancer le scrip.\e[0m" "\e[7Must be root to run this script.\e[0m")
+declare -a root_required=("\e[7mVous devez être en root pour lancer le scrip.\e[0m" "\e[7mMust be root to run this script.\e[0m")
 declare -a root_access=("\e[7mPermision Root : \e[32mOK\e[0m" "\e[7mRoot Permission : \e[32mOK\e[0m")
 declare -a check_update=("\e[7mVérification des mises à jour [1/3]...\e[0m" "\e[7mChecking for update [1/3]...\e[0m")
 declare -a done_update=("\e[7mVérification des mises à jour : \e[32mTerminées\e[0m" "\e[7mChecking for update : \e[32mFinish\e[0m")
@@ -103,7 +103,7 @@ then
     then
         echo "[$check_r] is not a number, run again the script and select a language under the list please !"
         exit $ERROR
-    elif [$check_r -ene 0 -o $check_r -ene 1]
+    elif [$check_r -ne 0 -o $check_r -ne 1]
     then
         read -p "[$check_r] are not on the list, please select a language under the list : " check_r
         echo
@@ -113,9 +113,15 @@ then
             exit $ERROR
         fi
     fi
-    LANGUAGE="$check_r"
 fi
-echo "Language set to : ${p_language["$LANGUAGE"]}"
+if [ $check_r -eq 1 ]
+then
+    LANGUAGE=1
+elif [ $check_r -eq 0 ]
+then
+    LANGUAGE=0
+fi
+echo "Language set to : ${p_language[$LANGUAGE]}"
 
 #===================================================================================#
 # just for output style #
@@ -135,11 +141,11 @@ echo
 echo
 if [ "$UID" -ene "$ROOT_UID" ]
 then
-    echo -e ${root_required["$LANGUAGE"]}
+    echo -e ${root_required[$LANGUAGE]}
     echo
     exit $E_NOTROOT
 else
-    echo -e ${root_access["$LANGUAGE"]}
+    echo -e ${root_access[$LANGUAGE]}
     echo
 fi
 
@@ -159,159 +165,159 @@ echo
 #===================================================================================#
 # Start update linux #
 echo
-echo -e ${check_update["$LANGUAGE"]}
+echo -e ${check_update[$LANGUAGE]}
 apt update
-echo -e ${done_update["$LANGUAGE"]}
+echo -e ${done_update[$LANGUAGE]}
 echo
-echo -e ${check_upgrade["$LANGUAGE"]}
+echo -e ${check_upgrade[$LANGUAGE]}
 apt -y upgrade --auto-remove --purge
-echo -e ${done_upgrade["$LANGUAGE"]}
+echo -e ${done_upgrade[$LANGUAGE]}
 echo
-echo -e ${check_upgradedist["$LANGUAGE"]}
+echo -e ${check_upgradedist[$LANGUAGE]}
 apt -y dist-upgrade --auto-remove --purge
-echo -e ${done_upgradedist["$LANGUAGE"]}
+echo -e ${done_upgradedist[$LANGUAGE]}
 echo
 
 #===================================================================================#
 # Start install required package for steamcmd #
 # Curl #
 echo
-echo -en ${request_curl["$LANGUAGE"]}
+echo -en ${request_curl[$LANGUAGE]}
 read check_r
 if ! [[ "$check_r" =~ $CH ]]
 then
-    echo -en ${remake_read["$LANGUAGE"]}
+    echo -en ${remake_read[$LANGUAGE]}
     read check_r
     echo
     if ! [[ "$check_r" =~ $CH ]]
     then
-        echo ${error_read["$LANGUAGE"]}
+        echo ${error_read[$LANGUAGE]}
         exit $ERROR
-    elif [["$check_r" =~ ${awnser_trad["$LANGUAGE"]}]]
+    elif [["$check_r" =~ ${awnser_trad[$LANGUAGE]}]]
     then
-        echo -en ${remake_read["$LANGUAGE"]}
+        echo -en ${remake_read[$LANGUAGE]}
         read check_r
         echo
-        if [["$check_r" =~ ${awnser_trad["$LANGUAGE"]}]]
+        if [["$check_r" =~ ${awnser_trad[$LANGUAGE]}]]
         then
-            echo ${error_read["$LANGUAGE"]}
+            echo ${error_read[$LANGUAGE]}
             exit $ERROR
         fi
     fi
 fi
 if [ "$check_r" == "o" ]
 then
-    echo -e ${install_curl["$LANGUAGE"]}
+    echo -e ${install_curl[$LANGUAGE]}
     apt -y install curl
-    echo -e ${done_curl["$LANGUAGE"]}
+    echo -e ${done_curl[$LANGUAGE]}
     echo
 elif [ "$check_r" != "n" ]
 then
-    echo -en ${remake_read["$LANGUAGE"]}
+    echo -en ${remake_read[$LANGUAGE]}
     read check_r
     echo
     if [ "$check_r" == "o" ]
     then
-        echo -e ${install_curl["$LANGUAGE"]}
+        echo -e ${install_curl[$LANGUAGE]}
         apt -y install curl
-        echo -e ${done_curl["$LANGUAGE"]}
+        echo -e ${done_curl[$LANGUAGE]}
         echo
     else
-        echo ${error_read["$LANGUAGE"]}
+        echo ${error_read[$LANGUAGE]}
         exit $ERROR
     fi
 fi
 # Nano #
 echo
-echo -en ${request_nano["$LANGUAGE"]}
+echo -en ${request_nano[$LANGUAGE]}
 read check_r
 if ! [[ "$check_r" =~ $CH ]]
 then
-    echo -en ${remake_read["$LANGUAGE"]}
+    echo -en ${remake_read[$LANGUAGE]}
     read check_r
     echo
     if ! [[ "$check_r" =~ $CH ]]
     then
-        echo ${error_read["$LANGUAGE"]}
+        echo ${error_read[$LANGUAGE]}
         exit $ERROR
-    elif [["$check_r" =~ ${awnser_trad["$LANGUAGE"]}]]
+    elif [["$check_r" =~ ${awnser_trad[$LANGUAGE]}]]
     then
-        echo -en ${remake_read["$LANGUAGE"]}
+        echo -en ${remake_read[$LANGUAGE]}
         read check_r
         echo
-        if [["$check_r" =~ ${awnser_trad["$LANGUAGE"]}]]
+        if [["$check_r" =~ ${awnser_trad[$LANGUAGE]}]]
         then
-            echo ${error_read["$LANGUAGE"]}
+            echo ${error_read[$LANGUAGE]}
             exit $ERROR
         fi
     fi
 fi
 if [ "$check_r" == "o" ]
 then
-    echo -e ${install_nano["$LANGUAGE"]}
+    echo -e ${install_nano[$LANGUAGE]}
     apt -y install nano
-    echo -e ${done_nano["$LANGUAGE"]}
+    echo -e ${done_nano[$LANGUAGE]}
     echo
 elif [ "$check_r" != "n" ]
 then
-    echo -en ${remake_read["$LANGUAGE"]}
+    echo -en ${remake_read[$LANGUAGE]}
     read check_r
     echo
     if [ "$check_r" == "o" ]
     then
-        echo -e ${install_nano["$LANGUAGE"]}
+        echo -e ${install_nano[$LANGUAGE]}
         apt -y install nano
-        echo -e ${done_nano["$LANGUAGE"]}
+        echo -e ${done_nano[$LANGUAGE]}
         echo
     else
-        echo ${error_read["$LANGUAGE"]}
+        echo ${error_read[$LANGUAGE]}
         exit $ERROR
     fi
 fi
 # Openssl #
 echo
-echo -en ${request_openssl["$LANGUAGE"]}
+echo -en ${request_openssl[$LANGUAGE]}
 read check_r
 if ! [[ "$check_r" =~ $CH ]]
 then
-    echo -en ${remake_read["$LANGUAGE"]}
+    echo -en ${remake_read[$LANGUAGE]}
     read check_r
     echo
     if ! [[ "$check_r" =~ $CH ]]
     then
-        echo ${error_read["$LANGUAGE"]}
+        echo ${error_read[$LANGUAGE]}
         exit $ERROR
-    elif [["$check_r" =~ ${awnser_trad["$LANGUAGE"]}]]
+    elif [["$check_r" =~ ${awnser_trad[$LANGUAGE]}]]
     then
-        echo -en ${remake_read["$LANGUAGE"]}
+        echo -en ${remake_read[$LANGUAGE]}
         read check_r
         echo
-        if [["$check_r" =~ ${awnser_trad["$LANGUAGE"]}]]
+        if [["$check_r" =~ ${awnser_trad[$LANGUAGE]}]]
         then
-            echo ${error_read["$LANGUAGE"]}
+            echo ${error_read[$LANGUAGE]}
             exit $ERROR
         fi
     fi
 fi
 if [ "$check_r" == "o" ]
 then
-    echo -e ${install_openssl["$LANGUAGE"]}
+    echo -e ${install_openssl[$LANGUAGE]}
     apt -y install openssl
-    echo -e ${done_openssl["$LANGUAGE"]}
+    echo -e ${done_openssl[$LANGUAGE]}
     echo
 elif [ "$check_r" != "n" ]
 then
-    echo -en ${remake_read["$LANGUAGE"]}
+    echo -en ${remake_read[$LANGUAGE]}
     read check_r
     echo
     if [ "$check_r" == "o" ]
     then
-        echo -e ${install_openssl["$LANGUAGE"]}
+        echo -e ${install_openssl[$LANGUAGE]}
         apt -y install openssl
-        echo -e ${done_openssl["$LANGUAGE"]}
+        echo -e ${done_openssl[$LANGUAGE]}
         echo
     else
-        echo ${error_read["$LANGUAGE"]}
+        echo ${error_read[$LANGUAGE]}
         exit $ERROR
     fi
 fi
@@ -319,38 +325,38 @@ fi
 #===================================================================================#
 # Steamcmd #
 echo
-echo -en ${request_steamcmd["$LANGUAGE"]}
+echo -en ${request_steamcmd[$LANGUAGE]}
 read check_r
 if ! [[ "$check_r" =~ $CH ]]
 then
-    echo -en ${remake_read["$LANGUAGE"]}
+    echo -en ${remake_read[$LANGUAGE]}
     read check_r
     echo
     if ! [[ "$check_r" =~ $CH ]]
     then
-        echo ${error_read["$LANGUAGE"]}
+        echo ${error_read[$LANGUAGE]}
         exit $ERROR
-    elif [["$check_r" =~ ${awnser_trad["$LANGUAGE"]}]]
+    elif [["$check_r" =~ ${awnser_trad[$LANGUAGE]}]]
     then
-        echo -en ${remake_read["$LANGUAGE"]}
+        echo -en ${remake_read[$LANGUAGE]}
         read check_r
         echo
-        if [["$check_r" =~ ${awnser_trad["$LANGUAGE"]}]]
+        if [["$check_r" =~ ${awnser_trad[$LANGUAGE]}]]
         then
-            echo ${error_read["$LANGUAGE"]}
+            echo ${error_read[$LANGUAGE]}
             exit $ERROR
         fi
     fi
 fi
 if [ "$check_r" == "o" ]
 then
-    echo -e ${install_gcc["$LANGUAGE"]}
+    echo -e ${install_gcc[$LANGUAGE]}
     apt-get -y install lib32gcc1
-    echo -e ${done_gcc["$LANGUAGE"]}
+    echo -e ${done_gcc[$LANGUAGE]}
     echo
-    echo -e ${install_sudo["$LANGUAGE"]}
+    echo -e ${install_sudo[$LANGUAGE]}
     apt-get -y install sudo
-    echo -e ${done_sudo["$LANGUAGE"]}
+    echo -e ${done_sudo[$LANGUAGE]}
     echo
     #===================================================================================#
     # Create sudo user #
@@ -368,7 +374,7 @@ then
 
     #===================================================================================#
     
-    echo -e ${user_create["$LANGUAGE"]}
+    echo -e ${user_create[$LANGUAGE]}
     echo
 
     #===================================================================================#
@@ -398,6 +404,6 @@ then
 
 elif [ "$check_r" != "n" ]
 then
-    echo ${error_read["$LANGUAGE"]}
+    echo ${error_read[$LANGUAGE]}
     exit $ERROR
 fi
